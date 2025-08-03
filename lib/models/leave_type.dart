0c89943c2bc7e8fc3585c48721b28cbfe895c675
arunofhyd <104165035+arunofhyd@users.noreply.github.com>
2025-08-03 20:16:02 +0530

    Delete lib directory

--- lib/models/leave_type.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class LeaveType {
-  String name;
-  double count;
-  Color color;
-
-  LeaveType({required this.name, required this.count, required this.color});
-}
